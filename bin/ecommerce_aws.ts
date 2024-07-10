389d8202692a3875f9074348053dc73a7d59225e
Leonardo Henrique Juliano da Silva <leonardojuliano16@gmail.com>
2024-07-10 13:40:17 -0300

    fixe .env

--- bin/ecommerce_aws.ts
+++ bin/ecommerce_aws.ts
@@ -3,10 +3,14 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ProductsAppStack } from '../lib/productsApp-stack';
 import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
+import * as dotenv from 'dotenv';
+
 
 const app = new cdk.App();
+dotenv.config();
 
-const env: cdk.Environment = { 
-  account: "533266991950",
-  region: "us-east-1"
+
+const env: cdk.Environment = {
+  account: process.env.account,
+  region: process.env.region
 }
